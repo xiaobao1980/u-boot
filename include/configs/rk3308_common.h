cc65f02c8150c3bc07361d17c76bb167f16ae132
ZHANG Yuntian <yt@radxa.com>
2023-04-26 14:51:21 +0800

    Update file loading address
    0x02800000 currently will be overwritten by kernel_addr_r
    on Radxa system (end at 0x02A3EA00), so move it out of this
    memory region.
    
    Also add fdtoverlay_addr_r to enable loading overlays.

--- include/configs/rk3308_common.h
+++ include/configs/rk3308_common.h
@@ -16,9 +16,12 @@
 #define ENV_MEM_LAYOUT_SETTINGS \
 	"scriptaddr=0x00500000\0" \
 	"pxefile_addr_r=0x00600000\0" \
-	"fdt_addr_r=0x02800000\0" \
+	"fdt_addr_r=0x1d000000\0" \
+	"fdtoverlay_addr_r=0x1f000000\0" \
 	"kernel_addr_r=0x00680000\0" \
-	"ramdisk_addr_r=0x04000000\0"
+	"ramdisk_addr_r=0x04000000\0" \
+	"kernel_comp_addr_r=0x10000000\0" \
+	"kernel_comp_size=0x2000000\0"
 
 #define CFG_EXTRA_ENV_SETTINGS \
 	ENV_MEM_LAYOUT_SETTINGS \
