90a1b2ea618af3be593432af7910a27b6c5a271c
ZHANG Yuntian <yt@radxa.com>
2023-10-24 17:08:12 +0800

    rock-pi-e: enable download mode

---
+++ include/configs/rock-pi-e-rk3328.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright (c) 2023 Radxa Ltd.
+ */
+
+#ifndef __ROCK_PI_E_RK3328_H
+#define __ROCK_PI_E_RK3328_H
+
+#include <configs/rk3328_common.h>
+
+#endif /* __ROCK_PI_E_RK3328_H */
