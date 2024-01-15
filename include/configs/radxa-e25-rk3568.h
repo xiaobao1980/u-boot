ac211f86dc2164395038d3752a307639aa5ab244
ZHANG Yuntian <95260730+RadxaYuntian@users.noreply.github.com>
2024-01-15 19:24:17 +0800

    radxa-e25: enable download mode

---
+++ include/configs/radxa-e25-rk3568.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright (c) 2024 Radxa Ltd.
+ */
+
+#ifndef __RADXA_E25_RK3568_H
+#define __RADXA_E25_RK3568_H
+
+#define ROCKCHIP_DEVICE_SETTINGS
+
+#include <configs/rk3568_common.h>
+
+#endif /* __RADXA_E25_RK3568_H */
